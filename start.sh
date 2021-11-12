c00a2f51b73376bcaaa5dd6f5422ffcb56f47830
aanper <mail@s3f.ru>
2021-11-12 12:47:28 +0300

    add start script

---
+++ start.sh
@@ -0,0 +1,2 @@
+export DISPLAY=:0
+chromium --kiosk --start-fullscreen --incognito --noerrdialogs --disable-translate --no-first-run --disable-infobars --window-position=0,0 --window-size=1920,1100 /boot/bydefault/drift/Drift.html
